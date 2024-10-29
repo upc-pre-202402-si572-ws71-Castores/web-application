e5a4ae391b61e70cc0272965705cc8967d5cf2e0
Rendery77 <rendery77@gmail.com>
2024-10-29 13:00:18 -0500

    feat(history-client): update styles

--- src/app/context/service-management/pages/history-client/history-client.component.ts
+++ src/app/context/service-management/pages/history-client/history-client.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import {MatButtonModule} from "@angular/material/button";
+import {MatDialogModule} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-history-client-page',
   templateUrl: './history-client.component.html',
   styleUrls: ['./history-client.component.css'],
   standalone: true, // Declaramos como independiente
-  imports: [CommonModule] // Importa el formulario
+    imports: [CommonModule,FontAwesomeModule,MatButtonModule, MatDialogModule] // Importa el formulario
 })
-export class HistoryClientPageComponent {}
+export class HistoryClientPageComponent {
+  faMapMarkerAlt = faMapMarkerAlt;
+}
