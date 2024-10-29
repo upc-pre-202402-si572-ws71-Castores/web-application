5701f601e039fcdac87746e0e5a65b5c51799460
Rendery77 <rendery77@gmail.com>
2024-10-28 22:08:01 -0500

    feat(page-not-found): redirect to home

--- src/app/public/pages/page-not-found/page-not-found.component.ts
+++ src/app/public/pages/page-not-found/page-not-found.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatIcon, MatIconModule} from "@angular/material/icon";
 import {MatDividerModule} from "@angular/material/divider";
 import {MatButtonModule} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [MatIcon,MatButtonModule, MatDividerModule, MatIconModule],
+  imports: [MatIcon,MatButtonModule, MatDividerModule, MatIconModule,RouterLink],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
