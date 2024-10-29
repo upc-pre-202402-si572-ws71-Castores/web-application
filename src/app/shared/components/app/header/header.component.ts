cf1b006369bc2669fa09a4989eb46fc4c36e1a8f
Rendery77 <rendery77@gmail.com>
2024-10-28 22:59:13 -0500

    feat(header): add flags languages

--- src/app/shared/components/app/header/header.component.ts
+++ src/app/shared/components/app/header/header.component.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterModule } from '@angular/router'; // Importa RouterModule para routerLink
+import { RouterModule } from '@angular/router';
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatFormFieldControl, MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatMenuModule} from "@angular/material/menu"; // Importa RouterModule para routerLink
 
 @Component({
   selector: 'app-header-app',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule,MatExpansionModule,MatFormFieldModule,MatInputModule,MatDatepickerModule,MatMenuModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderAppComponent {
   isCarrier: boolean = true;  // Aquí podrías condicionar según el rol del usuario
-
+  readonly panelOpenState = signal(false);
   get homeRoute() {
     return this.isCarrier ? '/app/carrier' : '/app/client';
   }
