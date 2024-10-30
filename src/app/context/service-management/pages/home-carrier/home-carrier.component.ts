5066701038660ffc0d463adbffbe4b9a2136b414
Rendery77 <rendery77@gmail.com>
2024-10-30 13:28:05 -0500

    feat(home-carrier): update style css

--- src/app/context/service-management/pages/home-carrier/home-carrier.component.ts
+++ src/app/context/service-management/pages/home-carrier/home-carrier.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-home-carrier-page',
   templateUrl: './home-carrier.component.html',
   styleUrls: ['./home-carrier.component.css'],
   standalone: true, // Declaramos como independiente
-  imports: [CommonModule] // Importa el formulario
+    imports: [CommonModule, MatIcon] // Importa el formulario
 })
 export class HomeCarrierPageComponent {}
