1885b3ec8d27788591a91adefbf50003dfe46d8c
Rendery77 <rendery77@gmail.com>
2024-10-30 13:20:28 -0500

    feat(home-client): update style css

--- src/app/context/service-management/pages/home-client/home-client.component.ts
+++ src/app/context/service-management/pages/home-client/home-client.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-home-client-page',
   templateUrl: './home-client.component.html',
   styleUrls: ['./home-client.component.css'],
   standalone: true, // Declaramos como independiente
-  imports: [CommonModule] // Importa el formulario
+  imports: [CommonModule,MatIcon] // Importa el formulario
 })
-export class HomeClientPageComponent {}
+export class HomeClientPageComponent {
+
+}
