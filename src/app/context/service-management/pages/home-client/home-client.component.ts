c8c89de30b561acdfed28858bd6209e60a90e30d
Rendery77 <rendery77@gmail.com>
2024-10-31 09:13:36 -0500

    feat(languages): added more traductions

--- src/app/context/service-management/pages/home-client/home-client.component.ts
+++ src/app/context/service-management/pages/home-client/home-client.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatIcon} from "@angular/material/icon";
+import {RouterLink} from "@angular/router";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-home-client-page',
   templateUrl: './home-client.component.html',
   styleUrls: ['./home-client.component.css'],
-  standalone: true, // Declaramos como independiente
-  imports: [CommonModule,MatIcon] // Importa el formulario
+  standalone: true,
+  imports: [CommonModule,MatIcon,RouterLink,TranslateModule]
 })
 export class HomeClientPageComponent {
 
--- src/app/public/pages/page-not-found/page-not-found.component.ts
+++ src/app/public/pages/page-not-found/page-not-found.component.ts
@@ -3,11 +3,12 @@ import {MatIcon, MatIconModule} from "@angular/material/icon";
 import {MatDividerModule} from "@angular/material/divider";
 import {MatButtonModule} from "@angular/material/button";
 import {RouterLink} from "@angular/router";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [MatIcon,MatButtonModule, MatDividerModule, MatIconModule,RouterLink],
+    imports: [MatIcon, MatButtonModule, MatDividerModule, MatIconModule, RouterLink, TranslateModule],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
