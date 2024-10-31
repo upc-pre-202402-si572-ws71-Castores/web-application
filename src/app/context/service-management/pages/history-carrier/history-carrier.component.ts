e4461baae82492c4974ac3a6fbcddd659413bde9
Edery Abanto <rendery77@gmail.com>
2024-10-31 12:58:31 -0500

    feat(history-carrier): added language

--- src/app/context/service-management/pages/history-carrier/history-carrier.component.ts
+++ src/app/context/service-management/pages/history-carrier/history-carrier.component.ts
@@ -4,13 +4,14 @@ import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import {MatButtonModule} from "@angular/material/button";
 import {MatDialogModule} from "@angular/material/dialog";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-history-carrier-page',
   templateUrl: './history-carrier.component.html',
   styleUrls: ['./history-carrier.component.css'],
-  standalone: true, // Declaramos como independiente
-    imports: [CommonModule,FontAwesomeModule,MatButtonModule, MatDialogModule] // Importa el formulario
+  standalone: true,
+    imports: [CommonModule,FontAwesomeModule,MatButtonModule, MatDialogModule,TranslateModule ]
 })
 export class HistoryCarrierPageComponent {
   faMapMarkerAlt = faMapMarkerAlt;
