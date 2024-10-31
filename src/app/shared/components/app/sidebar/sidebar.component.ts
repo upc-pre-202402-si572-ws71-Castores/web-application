68678d1f30d4186cb3f2a89bb6f34ed25c877d41
Rendery77 <rendery77@gmail.com>
2024-10-31 07:33:57 -0500

    fix: routes for sidebar

--- src/app/shared/components/app/sidebar/sidebar.component.ts
+++ src/app/shared/components/app/sidebar/sidebar.component.ts
@@ -2,28 +2,29 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { CommonModule } from '@angular/common'; // Importa el CommonModule para *ngIf y *ngFor
-import { RouterModule } from '@angular/router'; // Importa RouterModule para routerLink
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-sidebar-app',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, CommonModule, RouterModule  ],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, CommonModule, RouterModule, TranslateModule ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
 export class SidebarAppComponent {
-    isCarrier: boolean = false; // Aquí podrías condicionar según el rol del usuario
-  
+    isCarrier: boolean = false;
+
     carrierRoutes = [
-      { path: 'carrier', label: 'Dashboard', icon: 'home_outline' },
-      { path: 'carrier/see-offers', label: 'See offers', icon: 'local_offer_outline' },
-      { path: 'carrier/history', label: 'History', icon: 'history_outline' },
+      { path: 'carrier', label: 'dashboard', icon: 'home_outline' },
+      { path: 'carrier/see-offers', label: 'seeoffers', icon: 'local_offer_outline' },
+      { path: 'carrier/history', label: 'history', icon: 'history_outline' },
     ];
-  
+
     clientRoutes = [
-      { path: 'client', label: 'Dashboard', icon: 'home_outline' },
-      { path: 'client/create-request', label: 'Create Request', icon: 'add_circle_outline' },
-      { path: 'client/history', label: 'History', icon: 'history_outline' },
+      { path: 'client', label: 'dashboard', icon: 'home_outline' },
+      { path: 'client/create-request', label: 'createrequest', icon: 'add_circle_outline' },
+      { path: 'client/history', label: 'history', icon: 'history_outline' },
     ];
   }
