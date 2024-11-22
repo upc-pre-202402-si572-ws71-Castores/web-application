e38f0856b6799e1b47546e3d5989f1065ee24569
Rendery <rendery77@gmail.com>
2024-11-22 10:21:36 -0500

    fix: history offers client

--- src/app/context/service-management/pages/history-client/history-client.component.ts
+++ src/app/context/service-management/pages/history-client/history-client.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import {MatButtonModule} from "@angular/material/button";
 import {MatDialogModule} from "@angular/material/dialog";
 import {TranslateModule} from "@ngx-translate/core";
+import {TransportappService} from "../../../../services/transportapp.service";
 
 @Component({
   selector: 'app-history-client-page',
@@ -13,6 +14,28 @@ import {TranslateModule} from "@ngx-translate/core";
   standalone: true, // Declaramos como independiente
     imports: [CommonModule, FontAwesomeModule, MatButtonModule, MatDialogModule, TranslateModule] // Importa el formulario
 })
-export class HistoryClientPageComponent {
+export class HistoryClientPageComponent implements OnInit {
+  offers: any[] = []; // Almacena todas las ofertas obtenidas del backend
   faMapMarkerAlt = faMapMarkerAlt;
+  constructor(private transportAppService: TransportappService) {}
+
+  ngOnInit(): void {
+    this.fetchOffers();
+  }
+
+  fetchOffers(): void {
+    const maxOffers = 10; // Número máximo de ofertas a buscar
+    for (let offerId = 1; offerId <= maxOffers; offerId++) {
+      this.transportAppService.getOfferById(offerId).subscribe({
+        next: (offer) => {
+          if (offer) {
+            this.offers.push(offer);
+          }
+        },
+        error: (err) => {
+          console.warn(`Offer ID ${offerId} not found or error occurred`, err);
+        },
+      });
+    }
+  }
 }
